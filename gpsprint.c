40863361f77a4d28ca7d0096b736f67850ebdfe5
Wilson H <wilson.huu@gmail.com>
2017-11-05 12:55:25 -0800

    cleaned printing

--- gpsprint.c
+++ gpsprint.c
@@ -21,6 +21,7 @@ prints:
 void YourPrintDataFunction(struct gps_data_t* gpsdata)
 {
     char timebuffer[64];
+
     for (size_t i = 0; i < MAXCHANNELS; i++)
     {
         //fprintf(stderr, "Inside first for loop\n");
@@ -37,25 +38,30 @@ void YourPrintDataFunction(struct gps_data_t* gpsdata)
 
     if (gpsdata->satellites_visible != 0)
     {
+        memset(timebuffer, 0, sizeof(timebuffer));
+        fprintf(stderr, "%s", timebuffer);
+        (void)unix_to_iso8601(gpsdata->fix.time, timebuffer, sizeof(timebuffer));
+
         fprintf(stderr, "I found a satellite!");
+        fprintf(stdout, "Time: %s\n
+                         Longitude: %f %c\n
+                         Latitude: %f %c\n",
+                         timebuffer, gpsdata->fix.longitude, (gpsdata->fix.longitude < 0) ? 'W' : 'E',
+                         gpsdata->fix.latitude, (gpsdata->fix.latitude < 0) ? 'S' : 'N');
         for (size_t i = 0; i < MAX_POSSIBLE_SATS; i++)
         {
             if (i < gpsdata->satellites_visible)
             {
-                memset(timebuffer, 0, sizeof(timebuffer));
-                (void)unix_to_iso8601(gpsdata->fix.time, timebuffer, sizeof(timebuffer));
                 fprintf(stdout,
-				   " Time: %s    PRN: %3d\n  Elevation: %02d\n   Azimuth: %03d\n   SS: %02d\n    Longitude: %f %c    Latidtude: %f %c     Used: %c\n",
+				   "SatNo: %n\n PRN: %3d\n Elevation: %02d\n Azimuth: %03d\n SS: %02ddB\n    Used: %c\n",
+                   i,
 				   timebuffer,
                    gpsdata->PRN[i],
 				   gpsdata->elevation[i],
                    gpsdata->azimuth[i],
 				   (int)gpsdata->ss[i],
-                   gpsdata->fix.longitude,
-                   (gpsdata->fix.longitude < 0) ? 'W' : 'E' ,
-                   gpsdata->fix.latitude,
-                   (gpsdata->fix.latitude < 0) ? 'S' : 'N',
                     usedflags[i] ? 'Y' : 'N');
+                fprintf(stdout, "------------------------------------\n\n");
             }
         }
     }
